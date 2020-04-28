6e585c9674f6fe88c406f75c88bae762d895d988
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-04-28 16:33:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,11 +5,12 @@ class App < Sinatra::Base
     erb :reverse
   end
 
-  post '/reverse' do
-  puts params
+ post '/reverse' do
+  original_string = params["string"]
+  reversed_string = original_string.reverse
  
   erb :reversed
-  end
+end
 
   get '/friends' do
     # Write your code here!
